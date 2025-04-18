2a42c82683fb7af2e4b8047a1912b323ea5c5002
hafiiddd <hafid.akhyar0411@gmail.com>
2025-04-18 19:49:46 +0700

    menggabungkan year, month, dan date join ke satu variable joinDate yg bertipe local date

--- src/main/java/com/mycompany/mkpl/uts/Period.java
+++ src/main/java/com/mycompany/mkpl/uts/Period.java
@@ -4,14 +4,34 @@
  */
 package com.mycompany.mkpl.uts;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author hafid
  */
 public class Period {
-    private int yearJoined;
-    private int monthJoined;
-    private int dayJoined;
+    private LocalDate joindDate; 
     private int monthWorkingInYear;
+
+    public LocalDate getJoindDate() {
+        return joindDate;
+    }
+
+    public void setJoindDate(LocalDate joindDate) {
+        this.joindDate = joindDate;
+    }
+
+ 
+
+    public int getMonthWorkingInYear() {
+        return monthWorkingInYear;
+    }
+
+    public void setMonthWorkingInYear(int monthWorkingInYear) {
+        this.monthWorkingInYear = monthWorkingInYear;
+    }
+    
+    
 }
 
