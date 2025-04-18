500ea60894d299ea59a3ed20e6e080ed1f18d8f8
hafiiddd <hafid.akhyar0411@gmail.com>
2025-04-18 18:37:34 +0700

    refactor bad smell longmethod:membuat method untuk membantu perhitungan

--- src/main/java/com/mycompany/mkpl/uts/TaxFunction.java
+++ src/main/java/com/mycompany/mkpl/uts/TaxFunction.java
@@ -9,42 +9,42 @@ package com.mycompany.mkpl.uts;
  * @author hafid
  */
 public class TaxFunction {
-    /**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
-	
+
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+
+        validateNumberOfMonth(numberOfMonthWorking);
+
+        if (numberOfChildren > 3) {
+            numberOfChildren = 3;
+        }
+
+        int monthSalary = calculateMonthSalary(monthlySalary, otherMonthlyIncome, numberOfMonthWorking);
+        int calcuTax = calculateSalaryIncludeTax(isMarried, numberOfChildren);
+        int salaryAfterTax = monthSalary - deductible - calcuTax;
+
+        return salaryAfterTax;
+
+    }
+
+    public static void validateNumberOfMonth(int numberofWorking) {
+        if (numberofWorking > 12) {
+            System.err.println("More than 12 month working per year");
+        }
+    }
+
+    public static int calculateMonthSalary(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking) {
+        return (int) 0.5 * ((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking);
+    }
+
+    public static int calculateSalaryIncludeTax(boolean isMarried, int numberOfChildren) {
+        int salary = 54000000;
+        if (isMarried) {
+            salary += 4500000;
+            if (numberOfChildren > 0) {
+                salary += 4500000 * 3;
+            }
+        }
+
+        return salary;
+    }
 }
