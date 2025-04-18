c2a46db2026eb9d0b1cabe6baf45940b4a01059b
hafiiddd <hafid.akhyar0411@gmail.com>
2025-04-18 20:13:24 +0700

    memisahkan baseSalary dengan perhitungan monthSalary

--- src/main/java/com/mycompany/mkpl/uts/EmployeeIncome.java
+++ src/main/java/com/mycompany/mkpl/uts/EmployeeIncome.java
@@ -9,8 +9,57 @@ package com.mycompany.mkpl.uts;
  * @author hafid
  */
 public class EmployeeIncome {
-        private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
+
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int annualDeductible;
+
+    public EmployeeIncome(int monthlySalary, int otherMonthlyIncome, int annualDeductible) {
+        this.monthlySalary = monthlySalary;
+        this.otherMonthlyIncome = otherMonthlyIncome;
+        this.annualDeductible = annualDeductible;
+    }
+
+    public int getMonthlySalary() {
+
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(int grade, boolean isForeigner) {
+        int baseSalary = getBaseSalaryByGrade(grade);
+        if (isForeigner) {
+            baseSalary *= 1.5;
+        }
+        this.monthlySalary = baseSalary;
+    }
+
+    private int getBaseSalaryByGrade(int grade) {
+        switch (grade) {
+            case 1:
+                return 3000000;
+            case 2:
+                return 5000000;
+            case 3:
+                return 7000000;
+            default:
+                return 0;
+        }
+    }
+
+    public int getOtherMonthlyIncome() {
+        return otherMonthlyIncome;
+    }
+
+    public void setOtherMonthlyIncome(int otherMonthlyIncome) {
+        this.otherMonthlyIncome = otherMonthlyIncome;
+    }
+
+    public int getAnnualDeductible() {
+        return annualDeductible;
+    }
+
+    public void setAnnualDeductible(int annualDeductible) {
+        this.annualDeductible = annualDeductible;
+    }
 
 }
