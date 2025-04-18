cd6de8dd9b3f31efbeec57de7e8eae9b24e4f725
hafiiddd <hafid.akhyar0411@gmail.com>
2025-04-18 18:21:50 +0700

    first-commit

---
+++ src/main/java/com/mycompany/mkpl/uts/MkplUts.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mkpl.uts;
+
+/**
+ *
+ * @author hafid
+ */
+public class MkplUts {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
